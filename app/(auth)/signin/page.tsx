e06f6209120858d98996ebcd3d04261cf2ca72d0
Muhammad Umer <umerkang66@gmail.com>
2025-12-04 00:51:03 +0500

    add signin and signup

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+import { SigninView } from '@/app/modules/auth/ui/views/sign-in-view';
+
+const Signin = () => {
+  return <SigninView />;
+};
+
+export default Signin;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+import { SignupView } from '@/app/modules/auth/ui/views/sign-up-view';
+
+const Signup = () => {
+  return <SignupView />;
+};
+
+export default Signup;
